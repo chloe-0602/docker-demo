b70c772c4422747c855484751e8058f4c07ad8a9
HerryWEu <yihong.wu@mhp.com>
2024-11-16 15:45:39 +0800

    feat: 基础版本docker

---
+++ docker_boot/src/main/java/com/chloe/Main.java
@@ -0,0 +1,20 @@
+package com.chloe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.chloe
+ * Description:
+ *
+ * @Author Xu, Luqin
+ * @Create 2024/11/15 22:16
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
